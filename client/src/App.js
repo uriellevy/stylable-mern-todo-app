6b37956f3f7931043950e1fee8ae8d2d812ad2f4
uriel levy <uriellevy4892@gmail.com>
2021-10-29 09:15:57 +0300

    client styling and todo list component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo List</span> <br />
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   try {
     const task = await new Task(req.body).save();
-    res.send(task);
+    res.send(`${task.task} was added to the list`);
   } catch (error) {
     res.send(error);
   }
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
 router.patch("/:id", async (req, res) => {
   try {
     const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body);
-    res.send(task);
+    res.send(`task was updated successfully!`);
   } catch (error) {
     res.send(error);
   }
@@ -32,7 +32,7 @@ router.patch("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const task = await Task.findByIdAndDelete(req.params.id);
-    res.send(task);
+    res.send(`task with the name: "${task.task}" deleted`);
   } catch (error) {
     res.send(error);
   }
